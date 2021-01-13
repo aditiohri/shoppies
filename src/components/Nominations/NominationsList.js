43481ce1752050f921295794b8f9bdcf78a7c3c8
Aditi Ohri <didiohri@gmail.com>
2021-01-12 21:34:38 -0600

    set up basic component + container file structure

---
+++ src/components/Nominations/NominationsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NominationsList() {
+    return (
+        <div>
+            NominationsList
+        </div>
+    )
+}
---
+++ src/components/UI/SearchInput.js
@@ -0,0 +1,9 @@
+import React from 'react'
+//https://react-icons.github.io/react-icons/search?q=search
+export default function SearchInput() {
+    return (
+        <div>
+            SearchInput
+        </div>
+    )
+}
