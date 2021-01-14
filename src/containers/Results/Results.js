2ab2237ee012253c7e2d7dcb8d1e4527408c4bbb
Aditi Ohri <didiohri@gmail.com>
2021-01-13 21:13:30 -0600

    include error handling

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -7,24 +7,27 @@ import Paper from '@material-ui/core/Paper';
 export default class Results extends Component {
   state = {
     results: [],
-    loading: true
+    loading: true,
+    error: false
   };
 
   componentDidMount() {
-    console.log("Results.js mounted");
     fetch(`http://www.omdbapi.com/?s=Lord&type=movie&apikey=${process.env.REACT_APP_APIKEY}`)
       .then((response) => response.json())
       .then(movies => {
         const fetchedMovies = []
         for (let key in movies.Search) {
           const { Title, Year } = movies.Search[key]
-          // console.log(Title, Year)
           fetchedMovies.push({
             title: Title,
             year: Year
           })
         }
         this.setState({ results: fetchedMovies, loading: false })
+      })
+      .catch(error => {
+        console.log('error: ', error)
+        this.setState({ loading: false, error: true })
       });
   }
   render() {
