e2c3681b98f552268c6c5dbf999ed68e165e92f3
Aditi Ohri <didiohri@gmail.com>
2021-01-13 22:13:00 -0600

    use material ui textfield for searchbar input

--- src/components/UI/SearchInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-//https://react-icons.github.io/react-icons/search?q=search
-export default function SearchInput() {
-    return (
-        <>
-            SearchIcon+SearchInput
-        </>
-    )
-}
--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
-import SearchBar from "./SearchBar/SearchBar";
 
-import Paper from '@material-ui/core/Paper';
-
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
 export default class Search extends Component {
+  state = {
+    inputValue: "",
+  };
   render() {
     return (
       <>
         <h2>The Shoppies</h2>
-      <Paper>
-        <SearchBar />
-      </Paper>
+        <Paper>
+          <TextField variant="outlined" label="Movie Title" fullWidth />
+        </Paper>
       </>
     );
   }
