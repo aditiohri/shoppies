6bae8473954cd187ee971066366361e068a08b45
Aditi Ohri <didiohri@gmail.com>
2021-01-13 21:47:53 -0600

    style button using material ui

--- src/components/ResultsList/ResultItem/ResultItem.js
+++ src/components/ResultsList/ResultItem/ResultItem.js
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button'
 export default function ResultItem({ key, title, year }) {
     return (
         <li key={key}>
-            {title} ({year}) <Button />
+            {title} ({year}) <Button variant="outlined" size="small">Nominate</Button>
         </li>
     )
 }
