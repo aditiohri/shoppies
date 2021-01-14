b9847bfd91abf1a73863f21373cf5b29905a042e
Aditi Ohri <didiohri@gmail.com>
2021-01-13 20:37:29 -0600

    install dotenv

--- src/containers/Results/Results.js
+++ src/containers/Results/Results.js
@@ -5,6 +5,18 @@ import Paper from '@material-ui/core/Paper';
 
 
 export default class Results extends Component {
+  state = {
+    results: [],
+  };
+
+  componentDidMount() {
+    let data = {}
+    console.log("Results.js mounted");
+    fetch(`http://www.omdbapi.com/?s=Lord&type=movie&apikey=${process.env.REACT_APP_APIKEY}`)
+      .then((response) => response.json())
+      .then((json) => Object.assign(data, json))
+      .then(() => console.log("results: ", data));
+  }
   render() {
     return (
       <Paper>
