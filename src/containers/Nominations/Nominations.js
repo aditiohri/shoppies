ecdd99c11fd197d1898143d5eba581e10f054276
Aditi Ohri <didiohri@gmail.com>
2021-01-15 21:44:41 -0600

    user can now close banner

--- src/containers/Nominations/Nominations.js
+++ src/containers/Nominations/Nominations.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../store/actions/index";
 
 import Paper from "@material-ui/core/Paper";
 import NominationItem from "../../components/NominationsItem/NominationItem";
+import Banner from "../../components/UI/Banner";
 
 export default function Nominations() {
   const dispatch = useDispatch();
@@ -15,7 +16,6 @@ export default function Nominations() {
   const nominations = useSelector((state) => state.movies.nominations);
 
   let nominationsTitle, nominationsList;
-
   if (nominations.length > 0) {
     nominationsTitle = <h3>Nominations</h3>;
     nominationsList = nominations.map((movie, id) => (
@@ -30,10 +30,23 @@ export default function Nominations() {
     nominationsTitle = <h3>Nominate up to 5 movies!</h3>;
   }
 
+  const [openBanner, setBannerState] = useState(false);
+
+  const handleCloseBanner = () => {
+    setBannerState(false);
+  };
+
+  useEffect(() => {
+    if (nominations.length === 5) {
+      setBannerState(true)
+    }
+  }, [nominations.length])
+
   return (
     <Paper>
       {nominationsTitle}
       <ul>{nominationsList}</ul>
+      <Banner open={openBanner} handleClose={handleCloseBanner} />
     </Paper>
   );
 }
